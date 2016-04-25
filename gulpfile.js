0d9cb4869b776ec7935367772f205b0683a60fb7
IOriens <djjoriens@gmail.com>
2016-04-25 17:50:03 +0800

    fix bug in index

--- gulpfile.js
+++ gulpfile.js
@@ -19,18 +19,18 @@ var autoPrefixerOptions = {
 	browsers: ['last 2 versions', '> 10%']
 }
 
-gulp.task('sass',function() {
+gulp.task('sass', function () {
 	return gulp
 		.src(sassInput)
 		.pipe(sourcemaps.init())
-		.pipe(sass(sassOptions).on('error',sass.logError))
+		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(autoprefixer(autoPrefixerOptions))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(cssOuput))
 })
 
 //SassDoc generator
-gulp.task('sassdoc',function () {
+gulp.task('sassdoc', function () {
 	var sassdocOptions = {
 		dest: '/app/scss/sassdoc'
 	}
@@ -39,39 +39,51 @@ gulp.task('sassdoc',function () {
 		.pipe(sassdoc(sassdocOptions))
 		.resume()
 })
-gulp.task('watchSass',function () {
+gulp.task('watchSass', function () {
 	return gulp
-		.watch(sassInput,['sass'])
-		.on('change',function (event) {
+		.watch(sassInput, ['sass'])
+		.on('change', function (event) {
 			console.log('File： ' + event.path + ' was ' + event.type + ', running tasks...')
 		})
 })
 
 //BrowserSync
-gulp.task('serve',function(){
+gulp.task('serve', function () {
 	browserSync({
 		server: {
 			baseDir: 'app'
 		}
 	})
-	gulp.watch(['*.html','css/*.css','js/*.js'],{cwd: 'app'},reload)
+	gulp.watch(['*.html', 'css/*.css', 'js/*.js'], { cwd: 'app' }, reload)
 })
 
-//Production
-gulp.task('prod',['sassdoc'],function () {
-	return gulp
-		.src(sassInput)
-		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(autoprefixer(autoPrefixerOptions))
-		.pipe(gulp.dest(cssOuput))
-})
+
+
+
+
 
 //ESLint 检测JS的语法错误
-gulp.task('lint',function name() {
-	return gulp.src(['./app/js/*.js','./app/*.html'])
-	.pipe(eslint())
-	.pipe(eslint.format())	
+gulp.task('lint', function name() {
+	return gulp.src(['./app/js/*.js', './app/*.html'])
+		.pipe(eslint())
+		.pipe(eslint.format())
 })
 
 //Default Task
-gulp.task('default', ['sass', 'watchSass' ,'serve'/*, possible other tasks... */])
+gulp.task('default', ['sass', 'watchSass', 'serve'/*, possible other tasks... */])
+
+//Production 最后使用将可发布的文件放到dist文件夹中
+gulp.task('prod', ['sassdoc'], function () {
+	gulp.src(sassInput)
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(autoprefixer(autoPrefixerOptions))
+		.pipe(gulp.dest(cssOuput))
+	gulp.src(['./app/css/*.css','./app/css/*.map'])
+		.pipe(gulp.dest('./dist/css'))
+	gulp.src('./app/*.html')
+		.pipe(gulp.dest('./dist'))
+	gulp.src('./app/js/*.js')
+		.pipe(gulp.dest('./dist/js'))
+	gulp.src('./app/img/**/*')
+		.pipe(gulp.dest('./dist/img'))
+})
